a5db0027b968e605e18661446f69557f0e5d6918
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-07-08 10:01:14 -0400

    Update GameBoard logic - Update pokemon list to have 15 items - Create function to render cards from array - Create function to shuffle array items - Create instructions - Create and update state variables

--- src/GameBoard.jsx
+++ src/GameBoard.jsx
@@ -1,21 +1,30 @@
 import { useState, useEffect } from "react";
+import Score from "./Score.jsx";
 import Card from "./Card.jsx";
 
 const POKEMON_LIST = [
-  "sandshrew",
-  "gloom",
-  "umbreon",
-  "growlithe",
   "bellsprout",
-  "sentret",
-  "meowth",
+  "vileplume",
+  "grimer",
+  "weezing",
+  "garbodor",
+  "spinarak",
+  "weedle",
   "beedrill",
-  "psyduck",
-  "goldeen",
+  "ekans",
+  "dustox",
+  "venonat",
+  "nidorina",
+  "tentacool",
+  "gastly",
+  "zubat",
 ];
 
 function GameBoard() {
   const [pokemonData, setPokemonData] = useState([]);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [clickHistory, setClickHistory] = useState([]);
 
   useEffect(() => {
     async function fetchPokemon() {
@@ -37,19 +46,53 @@ function GameBoard() {
   }, []);
 
   function handleCardClick(id) {
-    console.log("hi");
+    shuffle(pokemonData);
+
+    if (!clickHistory.includes(id)) {
+      setClickHistory((prev) => [...prev, id]);
+      setScore((prev) => prev + 1);
+    } else {
+      setClickHistory([]);
+      if (score > highScore) {
+        setHighScore(score);
+      }
+      setScore(0);
+    }
+  }
+
+  function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  function renderCards() {
+    return pokemonData.map((pokemon) => (
+      <Card
+        key={pokemon.id}
+        name={pokemon.name}
+        image={pokemon.image}
+        onClick={() => handleCardClick(pokemon.id)}
+      />
+    ));
   }
 
   return (
     <div className="gameboard">
-      {pokemonData.map((pokemon) => (
-        <Card
-          key={pokemon.id}
-          name={pokemon.name}
-          image={pokemon.image}
-          onClick={() => handleCardClick(pokemon.id)}
-        />
-      ))}
+      <div className="instructions">
+        <p>
+          <span>How to play: </span>
+          In this game, you are presented with a grid of cards. Each time you
+          click a card you haven’t clicked before, your score increases by one.
+          However, if you click a card you’ve already selected, your score
+          resets to zero and the game restarts. The objective is to click
+          through all the cards once each, without repeating any, to achieve the
+          highest possible score.
+        </p>
+      </div>
+      <Score currentScore={score} highScore={highScore} pokemon={pokemonData} />
+      <div className="cards">{renderCards()}</div>
     </div>
   );
 }
